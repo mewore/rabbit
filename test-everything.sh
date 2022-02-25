12bca6664bbc318e48676b2a08a0c3737c9af346
mewore <46766863+mewore@users.noreply.github.com>
2022-02-26 00:18:43 +0200

    Use Gradle's '--parallel' parameter to parallelize the pre-push tasks
    This is way faster than spamming gradle executions. I should use this in the pipeline too. Unfortunately, it means I will have to compress everything into a single task, which will make it less readable.

--- test-everything.sh
+++ test-everything.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
-./gradlew core:jar
+tasks_to_run=('frontend:frontendLint' 'frontend:frontendTest' 'editor:jar' 'jar')
+for java_module in 'core' 'backend' 'editor'; do
+    tasks_to_run+=("${java_module}:spotbugsMain" "${java_module}:test")
+done
+
+echo "Running tasks: ${tasks_to_run[*]}"
+# shellcheck disable=SC2086
+./gradlew --parallel ${tasks_to_run[*]}
 result=$?
-
-process_ids=()
-for backend_module in 'core' 'backend' 'editor'; do
-    ./gradlew "${backend_module}:spotbugsMain" "${backend_module}:test" &
-    process_ids+=($!)
-done
-./gradlew frontend:frontendLint frontend:frontendTest &
-process_ids+=($!)
-./gradlew editor:jar &
-process_ids+=($!)
-./gradlew jar &
-process_ids+=($!)
-
-for pid in ${process_ids[*]}; do
-    wait "${pid}"
-    new_result=$?
-    if [[ "${result}" == '0' ]]; then
-        result="${new_result}";
-    fi
-done
 
 echo "Result: ${result}"
 exit $result
