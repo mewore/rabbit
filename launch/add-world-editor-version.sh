10af10c01f886c50eaa8e4957c5b754a71afa309
mewore <46766863+mewore@users.noreply.github.com>
2022-02-27 17:00:06 +0200

    Fix add-world-editor-version.sh

--- launch/add-world-editor-version.sh
+++ launch/add-world-editor-version.sh
@@ -5,13 +5,17 @@
 ./launch/require-env-vars.sh EDITOR_JAR_FILE SOURCE_BUILD_NUMBER || exit 1
 
 editor_checksum_file="${EDITOR_JAR_FILE}.md5"
-tmp_editor_checksum_file="${editor_checksum_file}.tmp"
+
+editor_jar_path="./editor/build/libs/${EDITOR_JAR_FILE}"
+if ! [ -e "${editor_jar_path}" ]; then
+    echo "ERROR: File '${editor_jar_path}' does not exist!"
+    exit 1
+fi
 
 echo 'Adding a new editor version if necessary...'
-editor_jar_path="./editor/build/libs/${EDITOR_JAR_FILE}"
-md5sum "${editor_jar_path}" | awk '{print $1;}' > "${tmp_editor_checksum_file}" || exit 1
-echo "Current checksum of ${EDITOR_JAR_FILE}: $(cat "${tmp_editor_checksum_file}")"
-if [ -e "${editor_checksum_file}" ] && grep "${editor_checksum_file}" < "${tmp_editor_checksum_file}"; then
+new_checksum=$(md5sum "${editor_jar_path}" | awk '{print $1;}')
+echo "Current checksum of ${EDITOR_JAR_FILE}: ${new_checksum}"
+if [ -e "${editor_checksum_file}" ] && grep "${new_checksum}" <"${editor_checksum_file}"; then
     echo "The old ${EDITOR_JAR_FILE} is the same as the new one (checksum: $(cat "${editor_checksum_file}"))"
     exit 0
 fi
@@ -33,4 +37,4 @@ windows_zip_path="${executable_dir}/windows/editor-win64.zip"
 mv "${windows_zip_path}" "${target_dir}/Rabbit World Editor v${SOURCE_BUILD_NUMBER} - win64.zip" || exit 1
 
 echo "Done moving the new version into ${target_dir}"
-mv "${tmp_editor_checksum_file}" "${editor_checksum_file}" || exit 1
+echo "${new_checksum}" >"${editor_checksum_file}"
