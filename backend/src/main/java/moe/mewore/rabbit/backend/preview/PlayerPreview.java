5834acebd336f25716b42f97a927896ba2eaad55
mewore <46766863+mewore@users.noreply.github.com>
2022-03-09 23:47:49 +0200

    [#41] Optimize the server preview
    Now, it doesn't redraw anything if nothing has changed. As a nice side effect, I also simplified the code in the process, which allowed me to remove the suppression of a complexity warning.
    
    Also, I made it so that the player name and the player latency are on two separate lines and have a semi-transparent background.

---
+++ backend/src/main/java/moe/mewore/rabbit/backend/preview/PlayerPreview.java
@@ -0,0 +1,36 @@
+package moe.mewore.rabbit.backend.preview;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@EqualsAndHashCode
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+class PlayerPreview {
+
+    private final int x;
+
+    private final int y;
+
+    private final int motionX;
+
+    private final int motionY;
+
+    private final int targetMotionX;
+
+    private final int targetMotionY;
+
+    private final String topText;
+
+    private final String bottomText;
+
+    public boolean hasTargetMotionLine() {
+        return (targetMotionX != x || targetMotionY != y) && (targetMotionX != motionX || targetMotionY != motionY);
+    }
+
+    public boolean hasMotionLine() {
+        return motionX != x || motionY != y;
+    }
+}
