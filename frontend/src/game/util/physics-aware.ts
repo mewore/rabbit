a0c6fb82b91f6ffa48b9922edd067e140196501e
mewore <46766863+mewore@users.noreply.github.com>
2022-03-11 13:27:38 +0200

    [#41] Split the updatable objects into render-aware and physics-aware ones

--- frontend/src/game/util/physics-aware.ts
+++ frontend/src/game/util/physics-aware.ts
@@ -1,8 +1,6 @@
-export interface Updatable {
+export interface PhysicsAware {
     readonly id: string | number;
 
     beforePhysics(delta: number, now: number): void;
     afterPhysics(delta: number, now: number): void;
-    update(delta: number, now: number): void;
-    beforeRender(delta: number, now: number): void;
 }
